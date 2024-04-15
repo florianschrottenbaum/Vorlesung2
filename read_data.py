ce116e68b30cdc1a11c0a8cfc6cd73b4aca9018d
TobiW3 <wt1651@mci4me.at>
2024-04-15 13:47:00 +0200

    edided read_data

--- read_data.py
+++ read_data.py
@@ -1,9 +1,11 @@
 import json
 
-# Opening JSON file
-file = open("data/person_db.json")
-
-# Loading the JSON File in a dictionary
-person_data = json.load(file)
-
-person_data
+def get_person_list():
+    # Opening JSON file
+    file = open("data/person_db.json")
+    # Loading the JSON File in a dictionary
+    person_data = json.load(file)
+    personlist = []
+    for counter in range(len(person_data)):
+        personlist.append(person_data[counter]["firstname"]+" "+(person_data[counter]["lastname"]))
+    return personlist
